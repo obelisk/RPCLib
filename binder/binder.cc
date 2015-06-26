d3e1ade9e16c91c97cc1ffef9a578d3f90f31699
Mitchell <mitchell.wm.grenier@gmail.com>
2015-06-26 13:56:10 -0400

    Moving some stuff around

---
+++ binder/binder.cc
@@ -0,0 +1,13 @@
+#include "../include/rpc.h"
+#include <vector>
+#include <string>
+#include <map>
+
+typedef struct{
+	int arguements;
+	
+} proc_sig_t;
+
+int main(int argc, char ** argv){
+	std::map<proc_sig_t, std::string> procedure_map;
+}
